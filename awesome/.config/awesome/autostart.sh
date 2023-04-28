57a48d5bd68384dba73af77fc73176b28aa6024f
fredrikscode <fredrik@kihlstedt.io>
2023-04-28 13:24:10 +0200

    Disabled dunst in autostart for now

--- awesome/.config/awesome/autostart.sh
+++ awesome/.config/awesome/autostart.sh
@@ -10,4 +10,4 @@ run() {
 run "feh" --randomize --bg-fill ~/.wallpapers/3840x1600
 run "$HOME/.config/polybar/launch.sh"
 run "picom"
-run "/usr/bin/dunst -conf $HOME/.config/dunst/dunstrc"
+#run "/usr/bin/dunst -conf $HOME/.config/dunst/dunstrc"
