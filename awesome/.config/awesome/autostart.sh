78002885260193ca79fb16b4e35b749dbae663a4
fredrikscode <fredrik@kihlstedt.io>
2023-05-01 16:04:53 +0200

    Add spotify-listener

--- awesome/.config/awesome/autostart.sh
+++ awesome/.config/awesome/autostart.sh
@@ -10,4 +10,5 @@ run() {
 run "feh" --randomize --bg-fill ~/.wallpapers/3840x1600
 run "$HOME/.config/polybar/launch.sh"
 run "picom"
+run "spotify-listener"
 #run "/usr/bin/dunst -conf $HOME/.config/dunst/dunstrc"
